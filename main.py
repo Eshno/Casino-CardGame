fa9007135acc31b398a98d54bcd3b6968a4b42a7
Eshno <oliver-felix@hotmail.com>
2019-04-12 14:00:07 -0400

    Players Creation

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ class Card:
         self.number = number
 
     def show(self):
-        return self.type, self.number
-
+        print self.type, self.number
 
 class Deck:
     cards = []
@@ -15,8 +14,8 @@ class Deck:
 
     def generate(self):
         for type in Deck.types:
-            for num in range(1, 14):
-                newCard = Card(type, num)
+            for number in range(1, 14):
+                newCard = Card(type, number)
                 Deck.cards.append(newCard)
     
     def shuffle(self):
@@ -25,16 +24,54 @@ class Deck:
             shuffledCard = random.choice(Deck.cards)
             shuffledDeck.append(shuffledCard)
         Deck.cards = shuffledDeck
+    
+    def generateAndShuffle(self):
+        self.generate()
+        self.shuffle()
+    
+    def deal(self):
+        dealedCard = Deck.cards.pop(0)
+        return dealedCard
             
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.cards = []
+        self.points = 0
+    
+    def showCards(self):
+        for card in self.cards:
+            print card.type , card.number
+    
+    def receiveCard(self, card):
+        self.cards.append(card)
+
+class Error:
+    errorMessage = ''
+    def isPlayerAmountValid(self, amount):
+        isValid = amount > 0 and amount <= 4 
+        if not isValid: 
+            Error.errorMessage = 'Player Amount is Not Valid'
+        return isValid
+
+def createPlayers(amount):
+    if not errorHandle.isPlayerAmountValid(amount):
+        print errorHandle.errorMessage
+        return
+    availablePlayerNames = ['Rose', 'Olive', 'Jorge', 'Maria']
+    players = []
+    for i in range(0, 4): 
+        players.append(Player(availablePlayerNames[i]))
+    return players
 
 
 deck = Deck()
-deck.generate()
+errorHandle = Error()
+deck.generateAndShuffle()
+playerAmount = 4
+players = createPlayers(playerAmount)
 
-# print random.choice(deck.cards).__dict__
+for player in players:
+    print player.__dict__
 
-# print len(deck.cards)
-
-print deck.shuffle()
-for card in deck.cards:
-    print card.type, card.number
+print deck.deal().__dict__
